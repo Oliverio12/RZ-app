63e2393369463db2674de547a801f448e732ee5d
rodri <rodrigozavaleta12@gmail.com>
2024-11-09 04:12:20 -0600

    Implementacion de Firebase e incorporacion de clases

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.android.application)
+    alias(libs.plugins.google.gms.google.services)
 }
 
 android {
@@ -32,11 +33,20 @@ android {
 }
 
 dependencies {
-
+    // nuevas dependencias
+    implementation(libs.circleimageview)
+    implementation(libs.firebase.auth)
+    implementation(libs.play.services.auth)
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(libs.firebase.firestore)
+
+    // Dependencias de Glide
+    implementation(libs.glide) // Glide
+    annotationProcessor(libs.glide.compiler) // Glide Compiler
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
---
+++ app/src/main/java/com/android/rz_app/TODOApplication.java
@@ -0,0 +1,15 @@
+package com.android.rz_app;
+
+import android.app.Application;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+public class TODOApplication extends Application{
+    public static FirebaseFirestore firebaseFirestore;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        firebaseFirestore = FirebaseFirestore.getInstance();
+    }
+}
+
